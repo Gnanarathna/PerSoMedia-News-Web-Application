fd415914405885e3a82ab88dc6730e5dfceb4158
Gnanarathna <sdhagnanarathna@gmail.com>
2026-02-27 17:50:01 +0530

    Frontend setup completed: React (Vite), Tailwind v3, React Router, folder structure initialized

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Categories from "../pages/Categories";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  );
+}
